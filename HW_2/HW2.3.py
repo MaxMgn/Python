7fbe1e05de6c7585c4d0fd926edb96cd16b204e1
MaxMgn <m17111987@gmail.ru>
2022-12-24 02:59:12 +0500

    HW2_second_stream

--- HW_2/HW2.3.py
+++ HW_2/HW2.3.py
@@ -1,6 +1,10 @@
 # Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
 n = int(input('Введите число: '))
-sum = 0
+dic = {}
 for i in range(1, n+1):
-    sum = (1 + 1/i) ** i + sum
-print(round(sum, 2))
+    dic[i] = round((1+1/i)**i,2)
+print(dic)
+print(sum(dic.values()))
+
+
+
--- HW_2/HW2.6.py
+++ HW_2/HW2.6.py
@@ -11,3 +11,16 @@ for _ in range (randint(1, 5)):
     i2 = randint(0, n-1)
     list[i1], list [i2] = list[i2], list [i1]
 print ("list after shuffling ", list)
+
+# import datetime
+# def random_int(num):
+#     rand = datetime.datetime.now().microsecond/10**6
+#     return int(num*rand)
+
+# a = [1,2,3,4,5,6]
+# print(a)
+# random_int(5)
+# for i in range(len(a)-1,-1,-1):
+#     j = random_int(i+1)
+#     a[i],a[j] = a[j],a[i]
+# print(a)

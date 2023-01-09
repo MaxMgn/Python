e636834f4c3dfd97b1267b0df97cd8a3276e9533
MaxMgn <m17111987@gmail.ru>
2023-01-09 23:00:44 +0500

    HW_4

--- HW_4/HW4.4.py
+++ HW_4/HW4.4.py
@@ -1,15 +1,15 @@
 import random
-# path = '/Users/mustinov/Documents/Programming/Python/HW4.4.txt'
+# path = '/Users/mustinov/Documents/Programming/Python/HW4/HW4_4.txt'
 k = input("Enter k: ")
 list = []
 with open ('HW4_4.txt', 'w') as data:
     list.append(random.randint(0, 100))
     if list[0] != 0:
-        data.write(str(list[0])+'*x^'+str(k))
+        data.write(str(list[0])+'*x**'+str(k))
     for i in range (1, k):
         list.append(random.randint(0, 100))
         if list[i] != 0:
-            data.write(' + ' + str(list[i])+'*x^'+str(k-i))
+            data.write(' + ' + str(list[i])+'*x**'+str(k-i))
     list.append(random.randint(0, 100))
     if list[k] != 0:
         data.write(' + ' + str(list[k]))
--- HW_4/HW4.5.py
+++ HW_4/HW4.5.py
@@ -3,33 +3,39 @@
 def polynom_file_to_list(file):
     with open (file, 'r') as data :
         for line in data:
-            polynom = line.replace(" = 0", "")
+            polynom = line.replace("**", "^").replace(" = 0", "")
             polynom = polynom.split("+")
             polynom[0].replace(" ", "")
-            degree_prev = int(polynom[0].split("^")[1])
+            print(polynom)
+            degree_highest = int(polynom[0].split("^")[1])
+            print(degree_highest)
             coeff_list = [int(polynom[0].split("*")[0])]
-            if len(coeff_list) != 1:
+            print(len(polynom))
+            if len(polynom) != 1:
                 for i in range (1, len(polynom)):
                     polynom[i].replace(" ", "")
                     if "^" not in polynom[i]:
                         degree = 0
                     else:
                         degree = int(polynom[i].split("^")[1])
-                    while degree_prev - degree != 1:
+                    while degree_highest - degree != 1:
                         coeff_list.append(0)
-                        degree_prev -= 1
-                    degree_prev = degree
+                        degree_highest -= 1
+                    degree_highest = degree
                     coeff_list.append(int(polynom[i].split("*")[0]))
-            else:
-                while degree_prev !=0:
+                while degree > 0:
                     coeff_list.append(0)
-                    degree_prev -=1
+                    degree -= 1
+            else:
+                while degree_highest !=0:
+                    coeff_list.append(0)
+                    degree_highest -=1
             coeff_list.reverse()
             return coeff_list   
     data.close()        
 
-coeff_listA = polynom_file_to_list("HW4_5_a.txt")
-coeff_listB = polynom_file_to_list("HW4_5_b.txt")
+coeff_listA = polynom_file_to_list('HW4_5_a.txt')
+coeff_listB = polynom_file_to_list('HW4_5_b.txt')
 while len(coeff_listA) != len(coeff_listB):
     if len(coeff_listA) > len(coeff_listB):
         coeff_listB.append(0)
@@ -45,10 +51,10 @@ print(coeff_listB)
 print(coeff_list_sum)
 
 with open ('HW4_5_sum.txt', 'w') as sum:
-    sum.write(str(coeff_list_sum[0]) +'*x^'+str(len(coeff_list_sum)-1))
+    sum.write(str(coeff_list_sum[0]) +'*x**'+str(len(coeff_list_sum)-1))
     for i in range (1, len(coeff_list_sum)-1):
         if coeff_list_sum[i] != 0:
-            sum.write(' + ' + str(coeff_list_sum[i])+'*x^'+str(len(coeff_list_sum)-i-1))
+            sum.write(' + ' + str(coeff_list_sum[i])+'*x**'+str(len(coeff_list_sum)-i-1))
     if coeff_list_sum[len(coeff_list_sum) - 1] != 0:
         sum.write(str(coeff_list_sum[len(coeff_list_sum) - 1]))
     sum.write(" = 0")
